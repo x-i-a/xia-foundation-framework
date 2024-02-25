152ff197777d42d44866e7a2c6ed3412867a5db4
X-I-A <develop@x-i-a.com>
2024-02-25 18:57:34 +0100

    feat: dependency management

--- xia_module/__init__.py
+++
@@ -1,7 +0,0 @@
-from xia_module.module import Module
-
-__all__ = [
-    "Module"
-]
-
-__version__ = "0.0.1"
--- xia_module_pypi/gcp.py
+++ xia_module_pypi/gcp.py
@@ -3,3 +3,4 @@ from xia_module import Module
 
 class PypiGcp(Module):
     module_name = "gcp-module-pypi"
+    activate_depends = ["gcp-module-project"]
