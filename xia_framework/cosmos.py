4c68eab161e986f64f4a892572c48aa0f6edb13a
X-I-A <develop@x-i-a.com>
2024-02-25 13:13:01 +0100

    feat: initialization

--- xia_framework/cosmos.py
+++ xia_framework/cosmos.py
@@ -9,13 +9,16 @@ class Cosmos(Framework):
         super().__init__(config_dir=config_dir, **kwargs)
         self.landscape_yaml = os.path.sep.join([self.config_dir, "landscape.yaml"])
 
-    def bigbang(self, cosmos_name: str, realm_name: str = None):
+    def bigbang(self, cosmos_name: str):
         """Create the cosmos administration project
 
         TODO:
             * Activate Cloud Billing API during cosmos project creation
             * Activate Cloud Resource Manager API during cosmos project creation
             * Activate Identity and Access Management during cosmos project creation
+
+        Args:
+            cosmos_name (str): Cosmos Name
         """
 
         with open(self.landscape_yaml, 'r') as file:
@@ -26,7 +29,6 @@ class Cosmos(Framework):
             raise ValueError("Realm project must be provided")
         if current_cosmos_name and current_cosmos_name != cosmos_name:
             raise ValueError("Realm project doesn't match configured landscape.yaml")
-        realm_name = cosmos_name if not realm_name else realm_name
         get_billing_cmd = f"gcloud billing accounts list --filter='open=true' --format='value(ACCOUNT_ID)' --limit=1"
         r = subprocess.run(get_billing_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
         current_settings["billing_account"] = r.stdout if "ERROR" not in r.stderr else None
@@ -36,11 +38,10 @@ class Cosmos(Framework):
         if cosmos_name in r.stdout:
             print(f"Realm Project {cosmos_name} already exists")
         else:
-            create_proj_cmd = f"gcloud projects create {cosmos_name} --name='{realm_name}'"
+            create_proj_cmd = f"gcloud projects create {cosmos_name} --name='{cosmos_name}'"
             r = subprocess.run(create_proj_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
             if "ERROR" not in r.stderr:
                 print(f"Realm Project {cosmos_name} create successfully")
-                current_settings["realm_name"] = realm_name
                 current_settings["cosmos_name"] = cosmos_name
                 with open(self.landscape_yaml, 'w') as file:
                     yaml.dump(landscape_dict, file, default_flow_style=False, sort_keys=False)
