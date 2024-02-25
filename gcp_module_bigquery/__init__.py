494cfe15ce51bff64427ba70883269f2896a9cb6
X-I-A <develop@x-i-a.com>
2024-02-25 21:05:19 +0100

    feat: pypi repository initialization

--- gcp_module_bigquery/__init__.py
+++
@@ -1,9 +0,0 @@
-from gcp_module_bigquery.table import Table
-from gcp_module_bigquery.dataset import Dataset
-
-__all__ = [
-    "Table",
-    "Dataset"
-]
-
-__version__ = "0.0.1"
--- xia_module_gcp_bigquery/dataset.py
+++ xia_module_gcp_bigquery/dataset.py
@@ -3,3 +3,4 @@ from xia_module import Module
 
 class Dataset(Module):
     module_name = "gcp-module-dataset"
+    activate_depends = ["gcp-module-project"]
