59f9bb3eb016b38d4455f795d88e040b58635996
X-I-A <develop@x-i-a.com>
2024-02-25 09:43:50 +0100

    feat: initialization

--- xia_module_gcp_project/templates/gcp-module-project/base/main.tf
+++ xia_module_gcp_project/templates/gcp-module-project/base/main.tf
@@ -5,5 +5,5 @@ module "gcp_module_project" {
   landscape_file = "../../../config/landscape.yaml"
   applications_file = "../../../config/applications.yaml"
 
-  depends_on = [module.gcp_module_application]
+  depends_on = [module.gh_module_application]
 }
